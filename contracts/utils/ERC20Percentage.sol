3755247221e4b08fe2023d9941141d7a4d2ca5c0
mr13tech <mr13@copernicspace.com>
2021-06-28 21:20:53 +0300

    removed commented code

--- contracts/utils/ERC20Percentage.sol
+++ contracts/utils/ERC20Percentage.sol
@@ -5,17 +5,6 @@ library ERC20Percentage {
 	using SafeMath for uint256;
 	uint256 public constant baseDecimal = 1e20;
 
-	//	function take(
-	//		uint256 percentage,
-	//		uint256 from,
-	//		uint256 decimals
-	//	) internal view returns (uint256 percent) {
-	//		uint256 normalized = percentage.mul(10**decimals).div(baseDecimal);
-	//		console.log(normalized);
-	//		percent = from.mul(normalized).div(10**decimals);
-	//		console.log(percent);
-	//	}
-
 	function take(
 		uint256 percentage,
 		uint256 amount,

233488257df5159b934189f5861ed4574e36fc68
Karol Sperczyński <sperczynskikarol@gmail.com>
2021-10-17 02:32:55 +0200

    SpacePayloadAsset WIP
    added spacePayloadAsset contract
    added unit tests

--- test/helpers/sell-id.helper.ts
+++
@@ -1,7 +0,0 @@
-import { BigNumber, ContractReceipt } from 'ethers'
-
-export const getSellID =  (sellAssetTxReceipt: ContractReceipt) =>
-	sellAssetTxReceipt.events
-	.filter((e) => e.event === 'CloseSmartOffer')
-	.map((e) => e.args.sellID)
-	.pop()

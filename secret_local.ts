7a920ab94fc20894a9af921d102af1676336469d
mr13tech <mr13@copernicspace.com>
2022-06-21 14:23:52 +0200

    add `secret ts module for gh test acation.

---
+++ secret_local.ts
@@ -0,0 +1,8 @@
+const MUMBAI_ALCHEMY_API = 'ZFv7QLmI21Ssn1yz9lcGk5-EE08pq5Jh'
+const MUMBAI_SEED = 'penalty sorry discover tissue curious wage purity monster mammal labor market lonely'
+// const MUMBAI_SEED = 'horse fashion sand post opera bamboo wing predict scorpion habit year donate'
+
+const POLYGON_ALCHEMY_API = 'gaam6u9PI3ybFmg4JVU1ilYO9Q-QWGTQ'
+const POLYGON_SEED = 'deal thunder lab race broken novel inject silly absurd detect entire argue'
+
+export { MUMBAI_ALCHEMY_API, MUMBAI_SEED, POLYGON_ALCHEMY_API, POLYGON_SEED }

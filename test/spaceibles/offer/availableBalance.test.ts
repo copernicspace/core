2e4d63ed34c9593090720d5421506d4fa1f19f71
Karol Sperczyński <sperczynskikarol@gmail.com>
2022-06-28 01:15:48 +0200

    Added `setupSpaceibleOffer` fixture & used in test
    Refactored `availableBalance` test to use the newly-added `setupSpaceibleOffer` fixture

--- test/spaceibles/offer/availableBalance.test.ts
+++ test/spaceibles/offer/availableBalance.test.ts
@@ -9,86 +9,28 @@ import { ERC20Mock, SpaceibleAsset, SpaceibleOffer } from '../../../typechain'
 import { getAssetID } from '../../helpers/getAssetId.helper'
 import { getOfferId } from '../../helpers/getOfferId.helper'
 import { deploySpaceibleOffer } from './fixtures/deploy.fixture'
+import { setupSpaceibleOffer } from './fixtures/setupOffer.fixture'
 import { Balances, balances } from './helpers/balances'
 
 const loadFixture: ReturnType<typeof waffle.createFixtureLoader> = waffle.createFixtureLoader()
 
 describe('[spaceibles/availableBalance] test set for available balance', () => {
-	let deployer: SignerWithAddress
-
+	let asset, offer
 	let spaceibleAsset: SpaceibleAsset
 	let spaceibleOffer: SpaceibleOffer
-
-	let seller: SignerWithAddress
-	let buyer: SignerWithAddress
-
+	let deployer, seller: SignerWithAddress
 	let money: ERC20Mock
-	let moneyDecimals: number
-
-	let buyTx: ContractTransaction
-
 	before(
-		'load offer/fixtures/deploy',
-		async () => ({ deployer, spaceibleAsset, spaceibleOffer } = await loadFixture(deploySpaceibleOffer))
-	)
-
-    before('load seller and buyer signers', async () => ([, seller, buyer] = await ethers.getSigners()))
-
-	before('deploy ERC20 Mock', async () => {
-		money = await ethers
-			.getContractFactory(contractNames.ERC20_MOCK)
-			.then(factory => factory.deploy())
-			.then(contract => contract.deployed())
-			.then(deployedContract => deployedContract as ERC20Mock)
-
-		moneyDecimals = await money.decimals()
-	})
-
-	const bps = 10000
-	const operatorFee = 300
-
-	const asset = {
-		id: undefined, // initialize after mint tx
-		cid: 'test-buy-tx',
-		balance: 100,
-		royalties: 0,
-		data: '0x'
-	}
-
-	before(
-		'grant creator role to user',
-		async () => await spaceibleAsset.connect(deployer).grantCreatorRole(seller.address)
-	)
-
-	before(
-		'mint asset as seller and assign id',
-		async () =>
-			await spaceibleAsset
-				.connect(seller)
-				.mint(asset.cid, asset.balance, asset.royalties, asset.data)
-				.then(tx => tx.wait())
-				.then(txr => (asset.id = getAssetID(txr)))
-	)
-
-	const offer = {
-		id: undefined, // initialize after sell tx
-		amount: asset.balance * 0.5, // sell max balance
-		price: parseUnits('100', moneyDecimals)
-	}
-
-	before(
-		'approve for all as seller',
-		async () => await spaceibleAsset.connect(seller).setApprovalForAll(spaceibleOffer.address, true)
-	)
-
-	before(
-		'create new offer as seller',
-		async () =>
-			await spaceibleOffer
-				.connect(seller)
-				.sell(asset.id, offer.amount, offer.price, money.address)
-				.then(tx => tx.wait())
-				.then(txr => (offer.id = getOfferId(txr)))
+		'load offer/fixtures/setupOffer',
+		async () => ({ 
+			asset, 
+			offer, 
+			spaceibleAsset, 
+			spaceibleOffer,
+			money,
+			deployer,
+			seller
+		} = await loadFixture(setupSpaceibleOffer))
 	)
 
     describe('offer creation: insufficient balance revert', async () => {

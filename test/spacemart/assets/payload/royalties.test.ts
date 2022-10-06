0c72ece37fe732b4334d8506d9b55bbb6d698388
mr13tech <mr13@copernicspace.com>
2022-10-06 19:27:54 +0200

    refactor: simplify test.

--- test/spacemart/assets/payload/royalties.test.ts
+++ test/spacemart/assets/payload/royalties.test.ts
@@ -1,77 +1,21 @@
 import { waffle } from 'hardhat'
 import { expect } from 'chai'
-import { ethers } from 'hardhat'
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { BigNumber } from '@ethersproject/bignumber'
 import { parseUnits } from '@ethersproject/units'
-import { KycRegister, PayloadAsset, PayloadFactory } from '../../../../typechain'
+import { PayloadAsset } from '../../../../typechain'
 import { createPayloadAssetWithRoyalties } from './fixtures/create.fixture'
 
 describe('[spacemart/assets/payload/royalties.test] SpacePayload asset with royalties', () => {
-	let userA: SignerWithAddress, userB: SignerWithAddress
-
-	before('load userA as signerWithAddress', async () => ([, , userA, userB] = await ethers.getSigners()))
-	let creator: SignerWithAddress
-	let payloadFactory: PayloadFactory
 	let payloadAsset: PayloadAsset
-	let kycContract: KycRegister
 
 	beforeEach(
 		'load fixtures/deploy`',
-		async () =>
-			({ payloadFactory, payloadAsset, creator, kycContract } = await waffle.loadFixture(
-				createPayloadAssetWithRoyalties
-			))
+		async () => ({ payloadAsset } = await waffle.loadFixture(createPayloadAssetWithRoyalties))
 	)
-
-	it('has correct uri', async () => {
-		const actual = await payloadAsset.uri(0)
-		const expected = 'ipfs://TEST_ROOT_CID'
-		expect(expected).to.be.eq(actual)
-	})
-
-	it('has correct name', async () => {
-		const actual = await payloadAsset.getName(0)
-		const expected = 'rootSpacePayloadName'
-		expect(expected).to.be.eq(actual)
-	})
-
-	it('has correct decimals', async () => {
-		const actual = await payloadAsset.decimals()
-		const expected = BigNumber.from(18)
-		expect(expected).to.be.eq(actual)
-	})
-
-	it('sets correct supply', async () => {
-		const actual = await payloadAsset.balanceOf(creator.address, 0)
-		const expected = parseUnits('3500', 18)
-		expect(expected).to.be.eq(actual)
-	})
 
 	it('has correct royalties percent', async () => {
 		const actual = await payloadAsset.royalties()
 		const decimals = await payloadAsset.decimals()
 		const expected = parseUnits('5', decimals)
 		expect(expected).to.be.eq(actual)
-	})
-
-	it('disallows non-factory-clients from creating payload', async () => {
-		await expect(
-			payloadFactory
-				.connect(userA)
-				.create('ipfs://', 'revert test asset', 18, 6000, kycContract.address, 5, false, 'CID_TEST')
-		).to.be.revertedWith('You are not allowed to create new SpaceAsset:Payload')
-	})
-
-	it('disallows non-managers from adding clients', async () => {
-		await expect(payloadFactory.connect(userA).addClient(userB.address)).to.be.revertedWith(
-			'Only factory owner & managers can add clients'
-		)
-	})
-
-	it('disallows non-factory-owners adding new factory managers', async () => {
-		await expect(payloadFactory.connect(userB).addManager(userA.address)).to.be.revertedWith(
-			'Only factory owner can add managers'
-		)
 	})
 })

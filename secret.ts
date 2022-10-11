e147952cea9410b2deba5df211a1b8bc6ea79c6b
mr13tech <mr13@copernicspace.com>
2022-10-11 15:04:13 +0200

    Revert "remove comments."
    This reverts commit 22e045d8cb76932d081ccba3b6b5bc40577adc87.

--- secret.ts
+++ secret.ts
@@ -1,9 +1,7 @@
-const MUMBAI_ALCHEMY_API = 'ZFv7QLmI21Ssn1yz9lcGk5-EE08pq5Jh'
-const MUMBAI_SEED =
-	'coast finish child duck festival document luxury sentence layer doctor uniform rabbit stone save ice course key sense educate hedgehog element canyon become first'
+const MUMBAI_ALCHEMY_API = ''
+const MUMBAI_SEED = ''
 
-const POLYGON_ALCHEMY_API = 'gaam6u9PI3ybFmg4JVU1ilYO9Q-QWGTQ'
-const POLYGON_SEED =
-	'behind winter bag sheriff pizza add pipe math damage appear they regret copper fee aim juice riot drama barrel must child audit shine trip'
+const POLYGON_ALCHEMY_API = ''
+const POLYGON_SEED = ''
 
 export { MUMBAI_ALCHEMY_API, MUMBAI_SEED, POLYGON_ALCHEMY_API, POLYGON_SEED }

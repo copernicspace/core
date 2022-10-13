1182a7744728dcd6e97c3e0080c629354da32f31
mr13tech <mr13@copernicspace.com>
2022-10-13 10:51:11 +0200

    update deploy scripts.

--- scripts/spacemart/escrowListing.deploy.ts
+++ scripts/spacemart/escrowListing.deploy.ts
@@ -1,17 +1,21 @@
-import ContractNames from '../../constants/contract.names'
-import { polygonScanLink } from '../../tasks/utils/polygonScanLink'
-
+import { parseUnits } from 'ethers/lib/utils'
 import hre, { ethers } from 'hardhat'
+
+import contractNames from '../../constants/contract.names'
+import { polygonScanLink } from '../../tasks/utils/polygonScanLink'
 
 const network = hre.network.name
 
 async function main() {
+	const [deployer] = await ethers.getSigners()
+
 	await ethers
-		.getContractFactory(ContractNames.PAYLOAD_ASSET)
-		.then(deployFactory => deployFactory.deploy('app.copernicspace.com/assets'))
+		.getContractFactory(contractNames.ESCROW_LISTING)
+		.then(factory => factory.deploy(deployer.address, parseUnits('3', 18)))
+		.then(contract => contract.deployed())
 		.then(deployedContract => deployedContract.deployTransaction.wait())
 		.then(txr => {
-			console.log('`SpaceMart::PayloadAsset` contract deployed')
+			console.log('`SpaceMart::EscrowListing` contract deployed')
 			console.log(`Address: ${txr.contractAddress}`)
 			console.log(`Block: ${txr.blockNumber}`)
 			console.log(`TX: ${polygonScanLink(txr.transactionHash, network)}`)
--- scripts/spacemart/payloadFactory.deploy.ts
+++ scripts/spacemart/payloadFactory.deploy.ts
@@ -1,13 +1,22 @@
-import { ethers } from 'hardhat'
-import ContractNames from '../constants/contract.names'
+import { polygonScanLink } from '../../tasks/utils/polygonScanLink'
+import ContractNames from '../../constants/contract.names'
+import hre, { ethers } from 'hardhat'
+
+const network = hre.network.name
 
 async function main() {
-	const assetLogicTemplateAddress = 'SET ME ;)'
-
+	const assetLogicTemplateAddress = '0x2c48e5F2Fe5f586eCaD9eA3B8375B4AF09EDc259'
+	console.log(`Using ${assetLogicTemplateAddress} as payload asset logic template`)
 	await ethers
 		.getContractFactory(ContractNames.PAYLOAD_FACTORY)
 		.then(deployFactory => deployFactory.deploy(assetLogicTemplateAddress))
-		.then(asset => console.log('Payload factory deployed to:', asset.address))
+		.then(deployedContract => deployedContract.deployTransaction.wait())
+		.then(txr => {
+			console.log('`SpaceMart::PayloadFactory` contract deployed')
+			console.log(`Address: ${txr.contractAddress}`)
+			console.log(`Block: ${txr.blockNumber}`)
+			console.log(`TX: ${polygonScanLink(txr.transactionHash, network)}`)
+		})
 }
 
 main()

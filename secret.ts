91e01d498c1f0f441a7669c08a41c891b22796ec
mr13tech <mr13@copernicspace.com>
2022-10-24 11:33:06 +0200

    update EscrowListing contract.

--- secret.ts
+++ secret.ts
@@ -1,5 +1,6 @@
-const MUMBAI_ALCHEMY_API = ''
-const MUMBAI_SEED = ''
+const MUMBAI_ALCHEMY_API = 'ZFv7QLmI21Ssn1yz9lcGk5-EE08pq5Jh'
+const MUMBAI_SEED =
+	'coast finish child duck festival document luxury sentence layer doctor uniform rabbit stone save ice course key sense educate hedgehog element canyon become first'
 
 const POLYGON_ALCHEMY_API = ''
 const POLYGON_SEED = ''
